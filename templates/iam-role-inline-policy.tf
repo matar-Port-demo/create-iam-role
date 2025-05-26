aa97e0c3fd4f99da7af35f68bb26c28394879f02
Matarp13 <38067747+Matarp13@users.noreply.github.com>
2025-05-26 18:27:55 +0300

    Create iam-role-inline-policy.tf

---
+++ templates/iam-role-inline-policy.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "example" {
+  name = "{{ role_name }}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "example_policy" {
+  name   = "policy-for-{{ role_name }}"
+  role   = aws_iam_role.example.id
+  policy = <<POLICY
+{{ policy_json }}
+POLICY
+}
