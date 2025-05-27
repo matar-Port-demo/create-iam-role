b6298da481804c90d0d58fdb2710c2273a0e4088
Matarp13 <38067747+Matarp13@users.noreply.github.com>
2025-05-27 08:40:35 +0300

    Add IAM role clvt-port-role-test15 (#11)
    Co-authored-by: getport-io[bot] <getport-io[bot]@users.noreply.github.com>

---
+++ resources/clvt-port-role-test15/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+}
+
+resource "aws_iam_role" "example" {
+  name = "clvt-port-role-test15"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "example_policy" {
+  name   = "policy-for-clvt-port-role-test15"
+  role   = aws_iam_role.example.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:ListAllMyBuckets",
+        "ec2:DescribeInstances"
+      ],
+      Resource = "*"
+    }]
+  })
+}
