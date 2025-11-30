544b89561eaf900cc2c2be7344adeef653842a36
Matarp13 <matarp13@gmail.com>
2025-11-30 16:08:47 +0200

    Add GKE cluster self-service workflows and Port actions

---
+++ templates/gke-cluster.tf
@@ -0,0 +1,97 @@
+provider "google" {
+  project = "{{ project_id }}"
+  region  = "{{ region }}"
+}
+
+resource "google_container_cluster" "cluster" {
+  name     = "{{ cluster_name }}"
+  location = "{{ region }}"
+  
+  # We remove default node pool to create a custom one
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  
+  # Network configuration
+  network    = "default"
+  subnetwork = "default"
+  
+  # Enable basic auth (for demo purposes - in production use more secure methods)
+  master_auth {
+    username = ""
+    password = ""
+    
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+  
+  # Node pool configuration
+  node_config {
+    machine_type = "{{ machine_type }}"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+    image_type   = "COS_CONTAINERD"
+    
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    
+    preemptible = false
+  }
+  
+  # Enable logging and monitoring
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  
+  # Disable legacy ABAC
+  enable_legacy_abac = false
+  
+  # Resource labels
+  resource_labels = {
+    environment = "production"
+    managed_by  = "terraform"
+  }
+}
+
+# Custom node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.cluster.name}-node-pool"
+  location   = "{{ region }}"
+  cluster    = google_container_cluster.cluster.name
+  node_count = {{ node_count }}
+  
+  node_config {
+    machine_type = "{{ machine_type }}"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+    image_type   = "COS_CONTAINERD"
+    
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    
+    preemptible = false
+    
+    labels = {
+      environment = "production"
+    }
+  }
+  
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+
+output "cluster_name" {
+  value = google_container_cluster.cluster.name
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.cluster.endpoint
+}
+
+output "cluster_region" {
+  value = "{{ region }}"
+}
+
